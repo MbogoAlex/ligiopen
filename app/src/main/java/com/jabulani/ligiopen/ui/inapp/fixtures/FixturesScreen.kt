306413668637fbf02ee56dc5bcb49e048a691706
Mbogo-anonymous <mbogoalex3@gmail.com>
2024-11-01 08:23:33 +0300

    fixtures screen

---
+++ app/src/main/java/com/jabulani/ligiopen/ui/inapp/fixtures/FixturesScreen.kt
@@ -0,0 +1,161 @@
+package com.jabulani.ligiopen.ui.inapp.fixtures
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.jabulani.ligiopen.R
+import com.jabulani.ligiopen.ui.theme.LigiopenTheme
+import com.jabulani.ligiopen.utils.screenFontSize
+import com.jabulani.ligiopen.utils.screenHeight
+import com.jabulani.ligiopen.utils.screenWidth
+
+@Composable
+fun FixturesScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                vertical = screenHeight(x = 16.0),
+                horizontal = screenWidth(x = 16.0)
+            )
+    ) {
+        LazyColumn {
+            items(10) {
+                FixtureItemCell(
+                    modifier = Modifier
+                        .padding(
+                            top = screenHeight(x = 8.0),
+                            bottom = screenHeight(x = 8.0)
+                        )
+                )
+                Spacer(modifier = Modifier.height(screenHeight(x = 4.0)))
+                HorizontalDivider()
+                Spacer(modifier = Modifier.height(screenHeight(x = 4.0)))
+            }
+        }
+    }
+}
+
+@Composable
+fun FixtureItemCell(
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        Box(
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            Column {
+                Text(
+                    text = "Aug. 20, 2024 11:40am",
+                    fontSize = screenFontSize(x = 14.0).sp
+                )
+                Spacer(modifier = Modifier.height(screenHeight(x = 4.0)))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.football_club),
+                        contentDescription = null
+                    )
+                    Spacer(modifier = Modifier.width(screenWidth(x = 3.0)))
+                    Text(
+                        text = "Kisumu FC",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = screenFontSize(x = 14.0).sp
+                    )
+                }
+                Spacer(modifier = Modifier.height(screenHeight(x = 4.0)))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.football_club),
+                        contentDescription = null
+                    )
+                    Spacer(modifier = Modifier.width(screenWidth(x = 3.0)))
+                    Text(
+                        text = "Obunga FC",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = screenFontSize(x = 14.0).sp
+                    )
+                }
+            }
+        }
+        Box(
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.End
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.End,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.location),
+                        contentDescription = null
+                    )
+                    Spacer(modifier = Modifier.width(screenWidth(x = 3.0)))
+                    Text(
+                        text = "Kisumu stadium",
+                        fontSize = screenFontSize(x = 14.0).sp
+                    )
+                }
+                Spacer(modifier = Modifier.height(screenHeight(x = 4.0)))
+                OutlinedButton(
+                    onClick = { /*TODO*/ }
+                ) {
+                    Text(
+                        text = "Buy Tickets",
+                        fontSize = screenFontSize(x = 14.0).sp
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun FixturesScreenPreview() {
+    LigiopenTheme {
+        FixturesScreen()
+    }
+}
--- app/src/main/java/com/jabulani/ligiopen/ui/inapp/news/NewsDetailsScreen.kt
+++ app/src/main/java/com/jabulani/ligiopen/ui/inapp/news/NewsDetailsScreen.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.ArrowBack
@@ -73,6 +75,7 @@ fun NewsDetailsScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .verticalScroll(rememberScrollState())
                 .padding(
                     horizontal = screenWidth(x = 16.0),
                     vertical = screenWidth(x = 16.0)
@@ -83,7 +86,9 @@ fun NewsDetailsScreen(
                 fontSize = screenFontSize(x = 16.0).sp,
                 fontWeight = FontWeight.Bold
             )
-            Spacer(modifier = Modifier.height(screenHeight(x = .0)))
+            Spacer(modifier = Modifier.height(screenHeight(x = 16.0)))
+            Divider()
+            Spacer(modifier = Modifier.height(screenHeight(x = 16.0)))
             Text(text = item.body)
         }
     }
